1085f6973e1b9adab2d97b0b0962e86cadd896d6
petek157 <petek157@hotmail.com>
2020-01-15 17:25:17 -0600

    Working react router

---
+++ src/Inbox.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Inbox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>Inbox</div>
+         );
+    }
+}
+ 
+export default Inbox;
